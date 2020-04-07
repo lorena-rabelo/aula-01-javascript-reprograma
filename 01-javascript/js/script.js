a767593f3585c3946754e19360076d83646a8977
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-04-07 17:28:26 -0300

    correção de erro

--- 01-javascript/js/script.js
+++ 01-javascript/js/script.js
@@ -7,7 +7,7 @@ alert('Olá, mundo!');
 const numero1 = prompt('Digite um número');
 const numero2 = prompt('Digite outro número');
 
-alert(`O resultado da conta é ${parseInt(numero1) + parseInt(numero2)}`);
+alert(`O resultado da conta é ${parseInt(numero1) + parseInt(numero2)}.`);
 
 // 3. Crie um algoritmo que receba nome, endereço e telefone, e depois mostre-os;
 
@@ -15,14 +15,14 @@ const nome = prompt(`Digite seu nome`);
 const endereco = prompt(`Digite seu endereço`);
 const telefone = prompt(`Digite seu telefone`);
 
-alert(`${nome}, ${endereco} e ${telefone}`);
+alert(`Nome:${nome}, endereço: ${endereco} e telefone: ${telefone}.`);
 
 // 4. Solicite o ano de nascimento do usuário, calcule sua idade e mostre-a;
 
 const ano = prompt('Digite seu ano de nascimento:')
 let idade = 2020 - ano;
 
-alert(`Sua idade é ${idade}`);
+alert(`Você tem ${idade} anos.`);
 
 
 // 5. Receba do usuários 3 números e calcule a média entre eles.
@@ -31,4 +31,4 @@ const num1 = prompt('Digite um número');
 const num2 = prompt('Digite mais um número');
 const num3 = prompt('Digite outro número');
 
-alert(`${(parseInt(num1) + parseInt(num2)  + parseInt(num3))}/3`);
+alert(`A média entre esses três número é: ${(parseInt(num1) + parseInt(num2) + parseInt(num3))/3}`);
